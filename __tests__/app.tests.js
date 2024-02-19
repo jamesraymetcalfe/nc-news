f2d285dbc2f7d3c1dd71b6c8763875f9aac58a7f
jamesraymetcalfe <jmetcalfe90@gmail.com>
2024-02-19 15:50:43 +0000

    refactored based on feedback

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -49,14 +49,13 @@ describe("/api/topics", () => {
 
 describe("/api/articles/:articles_id", () => {
   test("GET: 200 sends a single article to the client", () => {
-    const articleID = 2;
     return request(app)
-      .get(`/api/articles/${articleID}`)
+      .get("/api/articles/2")
       .expect(200)
       .then((response) => {
         const { article } = response.body;
         expect(article).toMatchObject({
-          article_id: articleID,
+          article_id: 2,
           title: expect.any(String),
           topic: expect.any(String),
           author: expect.any(String),
@@ -72,7 +71,8 @@ describe("/api/articles/:articles_id", () => {
       .get("/api/articles/9999")
       .expect(404)
       .then((response) => {
-        expect(response.body.msg).toBe("article does not exist");
+        const { msg } = response.body;
+        expect(msg).toBe("article does not exist");
       });
   });
   test("GET:400 sends an appropriate status and error message when given an invalid id", () => {
